4dcb9e85ebe86f08f6423c06b6d4a5e789e7d634
Lb-LC101 <lisabartfeld@gmail.com>
2021-04-17 11:51:15 -0500

    Created Category Class, Controller, Repository, and Index: Work In Progress - with errors

---
+++ src/main/java/org/launchcode/liftoffrecipetracker/controllers/CategoryController.java
@@ -0,0 +1,23 @@
+package org.launchcode.liftoffrecipetracker.controllers;
+
+import org.launchcode.liftoffrecipetracker.data.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+@Autowired
+private CategoryRepository categoryRepository;
+
+    @GetMapping
+    public String displayAllCategories(Model model) {
+        model.addAttribute("title", "All Categories");
+        //model.addAttribute("category", CategoryRepository.findAll());
+        return "category/index";
+    }
+}
---
+++ src/main/java/org/launchcode/liftoffrecipetracker/models/Category.java
@@ -0,0 +1,57 @@
+package org.launchcode.liftoffrecipetracker.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Category  {
+
+    //Class Variables
+    @Id
+    private int id;
+    // do we want to specify size for category names?
+    // @Size(min = 4, max = 25, message = 'Category name must be between 4 and 25 characters long')
+    // @NotBlank
+    private String name;
+
+//    Add this after Recipe class is completed/extended
+//    @OneToMany(mappedBy = "category")
+//    private final List<Recipe> recipes = new ArrayList<>();
+
+
+    //Constructors
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category() {}
+
+    //Methods
+
+
+
+    //Getters and Setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public List<Recipe> getRecipes() {
+//        return recipes;
+//    }
+}
