3b4507cb7a14d961bc138a7fbd316da4904bf363
Brigid C <b@c.com>
2021-04-20 19:46:44 -0500

    Changed variable to ingredient. Created index view for view all recipes.

--- src/main/java/org/launchcode/liftoffrecipetracker/controllers/RecipeController.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/controllers/RecipeController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 
@@ -22,21 +23,21 @@ public class RecipeController {
 	public String displayAllRecipes(Model model) {
 		model.addAttribute("title", "All Recipes");
 		model.addAttribute("recipes", recipeRepository.findAll());
-		return "index";
+		return "recipe/index";
 	}
 
 	@GetMapping("create")
 	public String displayAddRecipeForm(Model model){
 		model.addAttribute(new Recipe());
-		return "recipes/create";
+		return "recipe/create";
 	}
 
 	@PostMapping("add")
-	public String processAddRecipeForm(@ModelAttribute  Recipe newRecipe, Errors errors, Model model) {
+	public String processAddRecipeForm(@ModelAttribute @Valid Recipe newRecipe, Errors errors, Model model) {
 		if (errors.hasErrors()) {
 			model.addAttribute("title", "Add Recipe");
 			model.addAttribute("errorMsg", "Try Again, invalid data!");
-			return "recipes/add";
+			return "recipe/add";
 		}
 		recipeRepository.save(newRecipe);
 		return "redirect:";
@@ -50,7 +51,7 @@ public class RecipeController {
 		if (result.isPresent()) {
 			Recipe recipe = (Recipe) result.get();
 			model.addAttribute("recipe", recipe);
-			return "recipes/view";
+			return "recipe/view";
 		} else {
 			return "redirect:../";
 		}
--- src/main/java/org/launchcode/liftoffrecipetracker/models/Recipe.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/models/Recipe.java
@@ -1,41 +1,51 @@
 package org.launchcode.liftoffrecipetracker.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import java.util.ArrayList;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.HashMap;
 
 
 @Entity
 public class Recipe extends AbstractEntity {
 
-	private HashMap<String, String> ingredient;
+	private HashMap<String, String> ingredients;
 
+	@NotBlank(message = "Recipe name required.")
+	@NotNull
+	@Size(min=2,max=50, message = "Recipe name must be between 2 and 50 characters long.")
 	private String name;
+
+	@NotNull
+	@NotBlank
+	@Size(min=4, max=1000, message = "Directions are required.")
+	private String directions;
+
+	@NotBlank
+	@Min(value = 1, message = "Must be at least one serving.")
 	private int servings;
+
 	private int cookTime;
 	private int prepTime;
-	private String images;
+	private String image;
 
-	//@ManyToMany
-	private ArrayList<Recipe> recipes;
-
-	public Recipe(HashMap<String, String> ingredient, String name) {
-		this.ingredient = ingredient;
+	public Recipe(HashMap<String, String> ingredients, String name, String directions) {
+		this.ingredients = ingredients;
 		this.name = name;
+		this.directions= directions;
 	}
 
-
-	public Recipe(){}
-
-	public String getImages() {
-		return images;
+	public Recipe() {
 	}
 
-	public void setImages(String images) {
-		this.images = images;
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
 	}
 
 	public String getName() {
@@ -46,12 +56,20 @@ public class Recipe extends AbstractEntity {
 		this.name = name;
 	}
 
-	public HashMap<String, String> getIngredient() {
-		return ingredient;
+	public HashMap<String, String> getIngredients() {
+		return ingredients;
 	}
 
-	public void setIngredient(HashMap<String, String> ingredient) {
-		this.ingredient = ingredient;
+	public void setIngredients(HashMap<String, String> ingredient) {
+		this.ingredients = ingredients;
+	}
+
+	public String getDirections() {
+		return directions;
+	}
+
+	public void setDirections(String directions) {
+		this.directions = directions;
 	}
 
 	public int getServings() {
@@ -77,14 +95,6 @@ public class Recipe extends AbstractEntity {
 	public void setPrepTime(int prepTime) {
 		this.prepTime = prepTime;
 	}
-
-	public ArrayList<Recipe> getRecipes() {
-		return recipes;
-	}
-
-	public void setRecipes(ArrayList<Recipe> recipes) {
-		this.recipes = recipes;
-	}
-
 }
+
 
