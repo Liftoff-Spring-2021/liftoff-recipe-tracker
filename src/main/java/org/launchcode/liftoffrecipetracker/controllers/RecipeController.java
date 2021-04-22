ed88f63252f869357169bb1751849143f46244d6
Dylan Mullins <dylanemullins92@gmail.com>
2021-04-22 12:36:10 -0500

    Added create.html and process controller. Updated ingredients variable to simplify to string for now.

--- src/main/java/org/launchcode/liftoffrecipetracker/controllers/RecipeController.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/controllers/RecipeController.java
@@ -1,12 +1,18 @@
 package org.launchcode.liftoffrecipetracker.controllers;
 
 
+import org.launchcode.liftoffrecipetracker.models.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.launchcode.liftoffrecipetracker.data.RecipeRepository;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("recipes")
@@ -23,4 +29,23 @@ public class RecipeController {
 		return "recipe/index";
 	}
 
+	@GetMapping("create")
+	public String displayCreateRecipeForm(Model model) {
+		model.addAttribute("title", "Create a Recipe");
+		model.addAttribute(new Recipe());
+		return "recipe/create";
+	}
+
+	@PostMapping("create")
+	public String processCreateRecipeForm(@ModelAttribute @Valid Recipe recipe,
+	                                      Errors errors,
+	                                      Model model) {
+		if (errors.hasErrors()) {
+			model.addAttribute("title", "Create a Recipe");
+			return "recipe/create";
+		}
+
+		recipeRepository.save(recipe);
+		return "redirect:";
+	}
 }
