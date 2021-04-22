92fdde0f533004cd11e06226928eb817a3874689
Dylan Mullins <dylanemullins92@gmail.com>
2021-04-22 14:37:47 -0500

    Added basic recipe detail view and updated recipe controller.

--- src/main/java/org/launchcode/liftoffrecipetracker/controllers/RecipeController.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/controllers/RecipeController.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.launchcode.liftoffrecipetracker.data.RecipeRepository;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("recipes")
@@ -47,5 +45,20 @@ public class RecipeController {
 
 		recipeRepository.save(recipe);
 		return "redirect:";
+	}
+
+	@GetMapping("detail")
+	public String displayEventDetails(@RequestParam int recipeId,
+	                                  Model model) {
+		Optional<Recipe> result = recipeRepository.findById(recipeId);
+
+		if (result.isEmpty()) {
+			model.addAttribute("title", "Invalid Recipe ID: " + recipeId);
+		} else {
+			Recipe recipe = result.get();
+			model.addAttribute("title", "Recipe Details: " + recipe.getName());
+			model.addAttribute("recipe", recipe);
+		}
+		return "recipe/detail";
 	}
 }
