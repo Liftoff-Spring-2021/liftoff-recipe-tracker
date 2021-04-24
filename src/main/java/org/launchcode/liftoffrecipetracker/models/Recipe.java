7342b69c967112c54627f561af1cdbb0027080e7
Dylan Mullins <dylanemullins92@gmail.com>
2021-04-24 16:18:01 -0500

    Updated dependencies to Lucene backend, updated properties, began building annotation relationships for search feature

--- src/main/java/org/launchcode/liftoffrecipetracker/models/Recipe.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/models/Recipe.java
@@ -1,5 +1,7 @@
 package org.launchcode.liftoffrecipetracker.models;
 
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import org.launchcode.liftoffrecipetracker.data.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,8 +12,9 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.HashMap;
 
-
+//@Indexed creates an index of the schema for searching purposes. Only entities with @Indexed with be indexed.
 @Entity
+@Indexed
 public class Recipe extends AbstractEntity {
 	//properties
 	@NotBlank
@@ -22,6 +25,7 @@ public class Recipe extends AbstractEntity {
 	@NotBlank(message = "Recipe name required.")
 	@NotNull
 	@Size(min=2,max=50, message = "Recipe name must be between 2 and 50 characters long.")
+	@FullTextField
 	private String name;
 
 	@NotNull
