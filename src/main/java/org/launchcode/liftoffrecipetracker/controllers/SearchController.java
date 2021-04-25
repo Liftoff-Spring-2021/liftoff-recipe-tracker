613d217b1cf907cbc305311371aad6390994ae5f
Dylan Mullins <dylanemullins92@gmail.com>
2021-04-25 11:36:02 -0500

    Added RecipeSearchServiceImpl and updated SearchController, modified view to include results.html

--- src/main/java/org/launchcode/liftoffrecipetracker/controllers/SearchController.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/controllers/SearchController.java
@@ -1,7 +1,35 @@
 package org.launchcode.liftoffrecipetracker.controllers;
 
+import org.launchcode.liftoffrecipetracker.data.RecipeRepository;
+import org.launchcode.liftoffrecipetracker.models.Recipe;
+import org.launchcode.liftoffrecipetracker.service.RecipeSearchService;
+import org.launchcode.liftoffrecipetracker.service.RecipeSearchServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("search")
 public class SearchController {
+
+	@Autowired
+	private RecipeRepository recipeRepository;
+
+	@Autowired
+	private RecipeSearchServiceImpl recipeSearchService;
+
+	@PostMapping("results")
+	public String displaySearchResults(Model model,
+	                                   @RequestParam String searchTerm) {
+		Iterable<Recipe> recipes;
+		recipes = recipeSearchService.recipeSearch(searchTerm);
+
+		model.addAttribute("title", "Recipe Results");
+		model.addAttribute("recipes", recipes);
+
+		return "search/results";
+	}
 }
--- src/main/java/org/launchcode/liftoffrecipetracker/service/RecipeSearchServiceImpl.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/service/RecipeSearchServiceImpl.java
@@ -1,16 +1,33 @@
 package org.launchcode.liftoffrecipetracker.service;
 
 import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.launchcode.liftoffrecipetracker.models.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import java.util.List;
 
 public class RecipeSearchServiceImpl implements RecipeSearchService {
 
+	private final EntityManager entityManager;
+
 	@Autowired
-	private EntityManager entityManager;
+	public RecipeSearchServiceImpl(final EntityManagerFactory entityManagerFactory) {
+		this.entityManager = entityManagerFactory.createEntityManager();
+	}
+
+	@PostConstruct
+	public void initializeRecipeSearchIndex() {
+		try {
+			SearchSession searchSession = Search.session(entityManager);
+			searchSession.massIndexer().startAndWait();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
 
 	@Override
 	public List<Recipe> recipeSearch(String searchTerm) {
