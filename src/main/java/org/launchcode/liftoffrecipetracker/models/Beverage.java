718bd85bf4aee8cf9467176c90e2eb74119d93f2
Lb-LC101 <lisabartfeld@gmail.com>
2021-04-30 15:28:52 -0500

    Cleaned up Beverage & Category class code/comments, added beverage to navbar functionality, added footers to beverage views

--- src/main/java/org/launchcode/liftoffrecipetracker/models/Beverage.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/models/Beverage.java
@@ -2,30 +2,18 @@ package org.launchcode.liftoffrecipetracker.models;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
     public class Beverage extends AbstractRecommendations{
-    //public class Beverage extends AbstractEntity {
 
         //Class Variables
-//        @Size(min = 4, max = 50, message = "Beverage name must be between 4 and 50 characters long")
-//        @NotBlank
-//        @NotNull
-//        private String name;
 
     @ManyToMany(mappedBy = "beverages")
     private final List<Recipe> recipes = new ArrayList<>();
 
         //Constructors
-
-//        public Beverage(String name) {
-//            this.name = name;
-//        }
 
         public Beverage() {
         }
@@ -33,14 +21,6 @@ import java.util.List;
         //Methods
         //Getters and Setters
 
-
-//        public String getName() {
-//            return name;
-//        }
-//
-//        public void setName(String name) {
-//            this.name = name;
-//        }
     public List<Recipe> getRecipes() {
         return recipes;
     }
