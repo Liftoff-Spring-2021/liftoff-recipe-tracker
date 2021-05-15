b748c1b2c031dd358a399e3290b4cea76a291783
Lb-LC101 <lisabartfeld@gmail.com>
2021-05-15 18:58:43 -0500

    Added background to home page.  Started other enhancements not yet complete.

--- src/main/java/org/launchcode/liftoffrecipetracker/controllers/HomeController.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/controllers/HomeController.java
@@ -1,20 +1,25 @@
 package org.launchcode.liftoffrecipetracker.controllers;
 
+import org.launchcode.liftoffrecipetracker.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller("/")
 public class HomeController {
-
+	//User user = authenticationController.getUserFromSession(userSession);
 	@GetMapping
-	public String displayIndex() {
+	public String displayIndex()
+	{
 		return "redirect:/home";
 	}
 
 	@GetMapping("/home")
-	public String displayHome(Model model) {
+	public String displayHome(Model model, HttpSession userSession) {
 		model.addAttribute("title", "Liftoff Recipe Tracker");
+		//model.addAttribute("username", userSession);
 		return "index";
 	}
 
