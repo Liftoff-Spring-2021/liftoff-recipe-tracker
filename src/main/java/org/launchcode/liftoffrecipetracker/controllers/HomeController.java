2111d3e4a917ca78fc657b04ab811009b77ba207
Lb-LC101 <lisabartfeld@gmail.com>
2021-05-17 12:32:11 -0500

    Added list of recipes to home page

--- src/main/java/org/launchcode/liftoffrecipetracker/controllers/HomeController.java
+++ src/main/java/org/launchcode/liftoffrecipetracker/controllers/HomeController.java
@@ -1,10 +1,12 @@
 package org.launchcode.liftoffrecipetracker.controllers;
 
+import org.launchcode.liftoffrecipetracker.data.RecipeRepository;
 import org.launchcode.liftoffrecipetracker.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
@@ -14,7 +16,9 @@ public class HomeController {
 	@Autowired
 	AuthenticationController authenticationController;
 
-	//User user = authenticationController.getUserFromSession(userSession);
+	@Autowired
+	RecipeRepository recipeRepository;
+
 	@GetMapping
 	public String displayIndex()
 	{
@@ -22,10 +26,11 @@ public class HomeController {
 	}
 
 	@GetMapping("/home")
-	public String displayHome(Model model, HttpSession userSession) {
+	public String displayHome(@RequestParam(required = false) Integer recipeId, Model model, HttpSession userSession) {
 		User user = authenticationController.getUserFromSession(userSession);
 		model.addAttribute("title", "Liftoff Recipe Tracker");
 		model.addAttribute("user", user);
+		model.addAttribute("recipe", recipeRepository.findAll());
 
 		return "index";
 	}
